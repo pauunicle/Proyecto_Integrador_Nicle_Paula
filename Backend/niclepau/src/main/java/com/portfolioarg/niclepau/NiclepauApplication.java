d953c0be2dbfdb3b7914f11c036d8a2c8a028fd1
pauunicle <abrilpaulanicle@gmail.com>
2022-12-25 19:52:52 -0300

    cambio nombre

---
+++ Backend/niclepau/src/main/java/com/portfolioarg/niclepau/NiclepauApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioarg.niclepau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiclepauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiclepauApplication.class, args);
+	}
+
+}
---
+++ Backend/niclepau/src/test/java/com/portfolioarg/niclepau/NiclepauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioarg.niclepau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiclepauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
